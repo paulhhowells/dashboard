ed0ff02e17fde2d98adc6ab46907596a9d733eac
paulhhowells <=>
2019-06-23 21:56:08 +0100

    feat: created ui from create-react-app, & tweaked

---
+++ ui/src/components/App/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="page">
+      <header className="page__header">
+        Header
+      </header>
+      <main className="page__main">
+        Main
+      </main>
+      <footer className="page__footer">
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
