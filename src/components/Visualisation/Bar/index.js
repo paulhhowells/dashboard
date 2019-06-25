236790fd3f6ac12ea13c0590065176693f7bffc8
paulhhowells <=>
2019-06-25 08:12:12 +0100

    feat: imported mock data into bar to test that it is being fetched

--- src/components/Visualisation/Bar/index.js
+++ src/components/Visualisation/Bar/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Bar ({data, ...props}) {
-  return <div>Bar</div>;
+  console.log('data', data);
+
+  return (
+    <div>
+      Bar
+      <div>
+        {data && data.data.data.map(datum => <span key={datum}> {datum} </span>)}
+      </div>
+    </div>
+  );
 }
