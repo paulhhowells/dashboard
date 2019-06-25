d3ddb98606f081b4008f7c4657ca515a1a0d313e
paulhhowells <=>
2019-06-25 21:48:39 +0100

    feat: enabled widget props to add classNames

--- src/components/Widget/UiWidget/index.js
+++ src/components/Widget/UiWidget/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const UiWidget = ({className, ...props}) => (
-  <div className={className}>UI: {props.type}</div>
-);
+const UiWidget = ({className, ...props}) => {
+  className += (props.className) ? ' ' + props.className : '';
+
+  return (
+    <div className={className}>UI: {props.type}</div>
+  );
+};
 
 export default UiWidget;
