bdf7fdb28ba6846dbee7a96c923ace474dccf707
paulhhowells <=>
2019-06-30 22:23:34 +0100

    feat: replaced fetch, useEffect, & useState with useFetchJson hook

--- src/components/Visualisation/index.js
+++ src/components/Visualisation/index.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { useFetchJson } from '../../hooks';
 import Bar from './Bar/';
 import Pie from './Pie/';
 import Pulsar from './Pulsar/';
@@ -15,45 +16,15 @@ const Visualisation = ({visualisation, dataConfig, ...props}) => {
     throw new Error('Visualisation type not found: ' + visualisation.type);
   }
 
-  const [ response, setResponse ] = useState(false); // state
-
-  // TODO: in the future use Suspense once it supports data fetching.
-  useEffect(
-    () => {
-
-      fetch(dataConfig.url)
-        .then(res => res.json()) // TODO: maybe check and throw an error here?
-        .then(
-          (result) => {
-            setResponse({
-              isLoaded: true,
-              result: result
-            });
-
-            console.log('result', result);
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setResponse({
-              isLoaded: true,
-              error
-            });
-          }
-        );
-      },
-      [dataConfig]
-    );
+  const { json, loading } = useFetchJson(dataConfig.url);
 
   const VisualisationComponent = visualisationComponents[visualisation.type];
 
-  if (!response.isLoaded) {
+  if (loading) {
     return <div>Loading, have a cup of tea!</div>
-  } else if (response.result) {
+  } else if (json) {
     return <VisualisationComponent
-      response={response}
-      result={response.result}
+      result={json}
       visualisation={visualisation}
       {...props}
     />;
