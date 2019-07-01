da52dadad2c5da4d7759ccd6e0c6004602def77c
paulhhowells <=>
2019-07-01 21:47:22 +0100

    feat: added D3 rendering inside an SVG ref

--- src/components/Visualisation/Pie/index.js
+++ src/components/Visualisation/Pie/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import * as d3 from 'd3';
+import SVG from '../../SVG';
 
-export default function Pie ({data, ...props}) {
-  return <div>Pie</div>;
+export default function Pie ({result, ...props}) {
+  const nodeRef = useRef(null);
+  const { data } = result;
+
+  useLayoutEffect(() => {
+    d3.select(nodeRef.current)
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect");
+  }, [data]);
+
+  return (
+    <SVG
+      ref={nodeRef}
+      width="100"
+      height="100"
+      viewBox="0 0 100 100">
+      <rect fill="#ec0" width="100" height="100" />
+    </SVG>
+  )
 }
