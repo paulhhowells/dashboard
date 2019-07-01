fae0aa7285f491c5b476d8a28aeb9364602a67a4
paulhhowells <=>
2019-07-01 21:42:31 +0100

    feat: added an SVG functional component for use with D3

---
+++ src/components/SVG/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Don’t forget that SVG attributes need to be camelCased in React.
+ * e.g. viewbox => viewBox
+ * (Which would be unnecessary in pure SVG).
+ */
+export default React.forwardRef((props, ref) => (<svg
+    ref={ref}
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  />
+));
