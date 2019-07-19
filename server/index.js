cee0c33e841639f155011675bbe523421ffaf1e5
paulhhowells <p.h.howells@gmail.com>
2019-07-19 21:55:04 +0100

    chore: converted indentation to tabs

--- server/index.js
+++ server/index.js
@@ -11,65 +11,66 @@ app.get('/array', (req, res) => res.json(arrayPicker()));
 app.get('/gauge', (req, res) => res.json({ value: 9, min: 0, max: 10 }));
 
 app.get(
-  '/numerals',
-  (req, res) => res.json({
-    // TODO: add something about number of digits or decimal places?
-    dataset: [
-      {
-        name: 'Ratio',
-        value: 999,
-      },
-      {
-        name: 'Threshold',
-        value: 64,
-      },
-    ]
-  })
+	'/numerals',
+	(req, res) => res.json({
+		// TODO: add something about number of digits or decimal places?
+		dataset: [
+			{
+				name: 'Ratio',
+				value: 999,
+			},
+			{
+				name: 'Threshold',
+				value: 64,
+			},
+		]
+	})
 );
+
 
 let arrayChoiceIndex = 0;
 const arrayChoices = [
-  {
-    "name": "Array",
-    "dataset": [
-      { "label": "A", "value": 1},
-      { "label": "B", "value": 2},
-      { "label": "C", "value": 3},
-      { "label": "D", "value": 5},
-      { "label": "E", "value": 8},
-      { "label": "F", "value": 13},
-      { "label": "G", "value": 21},
-    ]
-  },
-  {
-    "name": "Array",
-    "dataset": [
-      { "label": "A", "value": 3},
-      { "label": "B", "value": 3},
-      { "label": "C", "value": 4},
-      { "label": "D", "value": 5},
-      { "label": "E", "value": 6},
-    ]
-  },
-  {
-    "name": "Array",
-    "dataset": [
-      { "label": "B", "value": 4},
-      { "label": "C", "value": 3},
-      { "label": "D", "value": 7},
-      { "label": "E", "value": 8},
-      { "label": "F", "value": 9},
-      { "label": "G", "value": 17},
-    ]
-  },
+	{
+		"name": "Array",
+		"dataset": [
+			{ "label": "A", "value": 1},
+			{ "label": "B", "value": 2},
+			{ "label": "C", "value": 3},
+			{ "label": "D", "value": 5},
+			{ "label": "E", "value": 8},
+			{ "label": "F", "value": 13},
+			{ "label": "G", "value": 21},
+		]
+	},
+	{
+		"name": "Array",
+		"dataset": [
+			{ "label": "A", "value": 3},
+			{ "label": "B", "value": 3},
+			{ "label": "C", "value": 4},
+			{ "label": "D", "value": 5},
+			{ "label": "E", "value": 6},
+		]
+	},
+	{
+		"name": "Array",
+		"dataset": [
+			{ "label": "B", "value": 4},
+			{ "label": "C", "value": 3},
+			{ "label": "D", "value": 7},
+			{ "label": "E", "value": 8},
+			{ "label": "F", "value": 9},
+			{ "label": "G", "value": 17},
+		]
+	},
 ];
 
 function arrayPicker () {
-  arrayChoiceIndex++;
+	arrayChoiceIndex++;
 
-  if (arrayChoiceIndex >= arrayChoices.length) {
-    arrayChoiceIndex = 0;
-  }
+	if (arrayChoiceIndex >= arrayChoices.length) {
+		arrayChoiceIndex = 0;
+	}
 
-  return arrayChoices[arrayChoiceIndex];
+	return arrayChoices[arrayChoiceIndex];
 }
